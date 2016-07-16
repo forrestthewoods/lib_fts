bd81f682c833b9a4d920903a3ddd02faff968911
Forrest Smith <forrestthewoods@gmail.com>
2016-07-16 16:54:04 -0700

    Added fts_amalgamate:
    - includes fts_amalgamate and full test example

--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ single-file public domain libraries
 
 lib | lang | doc | desc
 ---- | --- | --- | ---
+fts_amalgamate | [Python](https://github.com/forrestthewoods/lib_fts/blob/master/code/fts_amalgamate.py) | [blog](https://blog.forrestthewoods.com/solving-ballistic-trajectories-b0165523348c#.krla7uaz8) | Code amalgamation tool inspired by [SQLite](https://www.sqlite.org/amalgamation.html)
 fts_ballistic_projectile | [C# (Unity)](https://github.com/forrestthewoods/lib_fts/blob/master/code/fts_ballistic_trajectory.cs) | [blog](https://blog.forrestthewoods.com/solving-ballistic-trajectories-b0165523348c#.krla7uaz8), [demo](https://dl.dropboxusercontent.com/u/2152526/fts_ballistic_trajectory_web/index.html) | Solve ballistic trajectory firing angles
 fts_fuzzy_match | [C++](https://github.com/forrestthewoods/lib_fts/blob/master/code/fts_fuzzy_match.h), [JavaScript](https://github.com/forrestthewoods/lib_fts/blob/master/code/fts_fuzzy_match.js) | [readme](https://github.com/forrestthewoods/lib_fts/blob/master/docs/fuzzy_match.md), [blog](https://blog.forrestthewoods.com/reverse-engineering-sublime-text-s-fuzzy-match-4cffeed33fdb#.m9cmfqknc), [demo](https://s3-us-west-2.amazonaws.com/forrestthewoods.staticweb/lib_fts/tests/fuzzy_match/fts_fuzzy_match_test.html) | Fuzzy string matching inspired by Sublime Text
 
@@ -34,14 +35,10 @@ All source code is embedded with the following license:
 My initials; Forrest Thomas Smith.
 
 #### What language is used?
-Several! C++ is my baseline. Different or multiple languages are used where appropriate.
+Several! Different or multiple languages are used where appropriate.
 
 #### Why C++ and not C?
-Because "proper" open source C code is full of gross macros and defines. I'd rather write simple code in a C++ namespace and call it a day.
-
-Open source, cross-platform C often include a dozen defines and macros to let users turn knobs on or off resulting in a million permutations. An alternative solution is to provide the simplest possible implementation that can trivially modified by users to meet their specific needs. I prefer both writing and using the second.
-
-There are some cases where user modification is expected. For example, a string processing algorithm or anything involving 3d math. Users can and should use their existing string or vector/matrix libraries. In such cases I prefer to provide basic implementations that be easily modified by the user. I believe that to be better than crufty, inscrutible macro schemes.
+Because I'd rather write simple code in a C++ namespace and call it a day. It's the simplest possible implementation that can be modified by users to meet their specific needs. I'd like to avoid macro hell if possible.
 
 #### Is C++11 used?
 Core libs linked on this page are written in C++ but very near to C. 
---
+++ tests/amalgamate/msvc_include_scrape_zlib.py
@@ -0,0 +1,99 @@
+﻿# LICENSE
+#
+#   This software is dual-licensed to the public domain and under the following
+#   license: you are granted a perpetual, irrevocable license to copy, modify,
+#   publish, and distribute this file as you see fit.
+
+
+import os
+import re
+
+def fts_msvc_include_scrape(config):
+
+    source_pattern = r'\d>\s*(.*\.c(pp)?)$'
+    include_pattern = r'including file: (\s*)(.*)$'
+    ignore_pattern = r'program files'
+
+    # Open build output
+    print "Opening output file: {0}".format(config['msvc_build_output'])
+    build_output = open(config['msvc_build_output'], 'r')
+
+    # Track which files have already been included
+    source_files = set()
+    include_files = []
+    include_files_set = set() # Unordered for fast lookup
+
+    # Loop over build output line by line
+    for line in iter(build_output):
+
+        # Source file
+        match = re.search(source_pattern, line)
+        if match:
+            source_file = match.group(1)
+            source_files.add(source_file)
+            continue
+
+        # Include file
+        match = re.search(include_pattern, line)
+        if match:
+            level = len(match.group(1))
+            include_file = match.group(2)
+
+            # Continue if file has already been included
+            if include_file in include_files_set:
+                continue
+            
+            # Some files should be ignored
+            match = re.search(ignore_pattern, line, re.IGNORECASE)
+            if match:
+                continue
+
+            # Build relative path
+            include_file = os.path.relpath(include_file, config['source_root'])
+
+            # Skip repeats
+            if include_file in include_files_set:
+                continue
+            
+            # Store include file
+            include_files_set.add(include_file)
+            include_files.append([level, include_file])
+
+    
+
+
+    # Print source files with relative path
+    print "\nSource Files:"
+    for root, dirs, files in os.walk(config['source_root']):
+        for name in files:
+            if name in source_files:
+                absolute_path = os.path.join(root, name)
+                relative_path = os.path.relpath(absolute_path, config['source_root'])
+                print relative_path
+
+    # Print ordered include files
+    print "\nInclude Files:"
+    file_stack = []
+    last_level = -1
+    for level, include_file in include_files:
+        while file_stack and file_stack[-1][0] >= level:
+            stack_level, stack_file = file_stack.pop()
+            print stack_file
+
+        file_stack.append([level, include_file])
+        last_level = level
+    
+    while file_stack:
+        stack_level, stack_file = file_stack.pop()
+        print stack_file
+
+
+def main():
+    config = {}
+    config['msvc_build_output'] = "msvc_build_output_zlib.txt"
+    config['source_root'] = 'c:\\temp\zlib\\'
+    fts_msvc_include_scrape(config)
+
+
+if __name__ == "__main__":
+    main()
